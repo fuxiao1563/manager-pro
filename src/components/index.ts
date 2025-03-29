07fb44f03e9b1ef071a256e176814d16e12ebe37
fuxiao1563 <2459009106@qq.com>
2025-03-29 20:39:34 +0800

    feat: add-fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import SvgIcon from './SvgIcon/index.vue'
+// 引入element-plus图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const allGlobalComponents: any = {
   SvgIcon,
 }
@@ -7,5 +9,9 @@ export default {
     Object.keys(allGlobalComponents).forEach((key) => {
       app.component(key, allGlobalComponents[key])
     })
+    //将element-plus提供全部图标注册为全局组件 
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component)
+    }
   },
 }
