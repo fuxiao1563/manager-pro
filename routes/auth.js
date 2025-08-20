3748f74f3705be38edd30c10e966fde9ebed2f90
fuxiao1563 <2459009106@qq.com>
2025-08-20 19:14:22 +0800

    feat: updata

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const authMiddleware = require('../middlewares/auth-middleware');
 const authController = require('../controllers/auth-controller')
 
 
 // 登录
-router.post('/login', authMiddleware(), authController.login)
+router.post('/login', authController.login)
 // 注册
-router.post('/register', authMiddleware(), authController.register)
+router.post('/register', authController.register)
 // 退出登录
-router.post('/logout', authMiddleware(), authController.logout)
+router.post('/logout',  authController.logout)
 
 
 module.exports = router;
